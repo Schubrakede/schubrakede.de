973f22a5f50cf89d83df4ad5d77fe0db7549241b
Lasse Diercks <github@lassediercks.de>
2019-07-15 21:06:21 +0200

    big bang

---
+++ gridsome.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  siteName: "Schubrakede",
+  transformers: {
+    remark: {
+      externalLinksTarget: "_blank",
+      externalLinksRel: ["nofollow", "noopener", "noreferrer"],
+      anchorClassName: "icon icon-link",
+      plugins: [
+        // ...global plugins
+      ]
+    }
+  },
+
+  plugins: [
+    {
+      use: "@gridsome/source-filesystem",
+      options: {
+        path: "content/blog/**/*.md",
+        typeName: "Blog",
+        remark: {
+          plugins: [
+            // ...local plugins
+          ]
+        }
+      }
+    },
+    {
+      use: `gridsome-plugin-netlify-cms`,
+      options: {
+        publicPath: `/admin`
+      }
+    }
+  ]
+};
