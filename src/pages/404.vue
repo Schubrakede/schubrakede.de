1dbb6888baa93524a2de926d1a3170f039851529
Lasse Diercks <github@lassediercks.de>
2019-08-16 15:14:02 +0200

    add 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,26 @@
+<template>
+  <Layout>
+    <Headline :level="1">404, Seite nicht gefunden</Headline>
+    <Spacer :size="1"></Spacer>
+    <Headline :level="3">"Nicht jeder Link liefert auch eine Webseite aus."</Headline>
+    <Paragraph>
+      <i>Alte DevOps Weisheit</i>
+    </Paragraph>
+    <Spacer :size="1"></Spacer>
+    <Paragraph>
+      Vielleicht möchtest du zurück
+      <AnchorLink href="/">auf die Startseite</AnchorLink>
+    </Paragraph>
+  </Layout>
+</template>
+
+<script>
+import Spacer from "../components/Spacer";
+import AnchorLink from "../components/AnchorLink";
+import Paragraph from "../components/Paragraph";
+import Headline from "../components/Headline";
+export default { components: { Headline, AnchorLink, Spacer, Paragraph } };
+</script>
+
+<style>
+</style>
