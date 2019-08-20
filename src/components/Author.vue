b10e38221eeaf68b9e001ae1185e473bc8c54d83
Lasse Diercks <github@lassediercks.de>
2019-08-20 22:03:36 +0200

    feat(Blog): add author

---
+++ src/components/Author.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="wrap">
+    <div class="author-image" :style="{backgroundImage: `url(${image})`}" />
+    <div>
+      <Headline :level="3">{{name}}</Headline>
+      <Paragraph>{{description}}</Paragraph>
+      <AnchorLink :href="`https://twitter.com/${twitterhandle}`">@{{twitterhandle}} auf Twitter</AnchorLink>
+    </div>
+  </div>
+</template>
+
+<script>
+import AnchorLink from "./AnchorLink";
+import Headline from "./Headline";
+import Paragraph from "./Paragraph";
+
+export default {
+  components: { Paragraph, AnchorLink, Headline },
+  props: ["name", "description", "image", "twitterhandle"]
+};
+</script>
+
+<style>
+.wrap {
+  margin: 3em 0;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-gap: 1em;
+}
+.author-image {
+  width: 150px;
+  height: 150px;
+  border-radius: 150px;
+  background-size: cover;
+}
+</style>
--- src/templates/Blog.vue
+++ src/templates/Blog.vue
@@ -1,17 +1,26 @@
 <template>
   <Layout>
-    {{$page.blog.author}}
     <Headline class="blog-headline" :level="1">{{$page.blog.headline}}</Headline>
     <RichText class="blog-content">{{$page.blog.content}}</RichText>
+    <Author
+      v-for="(author, index) in $page.allAuthor.edges[0]"
+      :key="index"
+      :name="author.name"
+      :image="author.image"
+      :description="author.description"
+      :twitterhandle="author.twitterhandle"
+    ></Author>
   </Layout>
 </template>
 
 <script>
 import RichText from "../components/RichText";
+import Author from "../components/Author";
 import Headline from "../components/Headline";
 export default {
   components: {
     Headline,
+    Author,
     RichText
   },
   metaInfo() {
@@ -28,16 +37,24 @@ export default {
 </script>
 
 <page-query>
-query Blog ($id: String!) {
+query Blog ($id: String!, $author: String!) {
   blog: blog (id: $id){
       headline,
       content,
       published,
       author,
+  },
+  allAuthor (filter: { twitterhandle: { eq: $author }}){
+    edges{
+      node{
+        name,
+        image,
+        twitterhandle,
+        description
+      }
+    }
   }
 }
-
-
 </page-query>
 
 <style lang="postcss">
@@ -48,6 +65,10 @@ query Blog ($id: String!) {
 .blog-content {
   img {
     max-width: 30em;
+    width: 100%;
+    margin: 0 auto;
+    display: block;
   }
+  margin-bottom: 5em;
 }
 </style>
