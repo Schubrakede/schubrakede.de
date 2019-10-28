43834f9998cd5d088350fec83cceb40214b82f2d
Lasse Diercks <github@lassediercks.de>
2019-10-28 21:58:18 +0100

    async font loading

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -87,10 +87,6 @@ export default {
           rel: "icon",
           type: "image/png",
           href: "/favicon_16-16.png"
-        },
-        {
-          rel: "stylesheet",
-          href: "https://use.typekit.net/uaf4juj.css"
         }
       ],
       title: this.title,
@@ -119,6 +115,37 @@ export default {
       ],
       style: [{ cssText: `:root\{${resolveColors(colors.values)}}` }]
     };
+  },
+  mounted() {
+    (function(d) {
+      var config = {
+          kitId: "uaf4juj",
+          scriptTimeout: 3000,
+          async: true
+        },
+        h = d.documentElement,
+        t = setTimeout(function() {
+          h.className =
+            h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+        }, config.scriptTimeout),
+        tk = d.createElement("script"),
+        f = false,
+        s = d.getElementsByTagName("script")[0],
+        a;
+      h.className += " wf-loading";
+      tk.src = "https://use.typekit.net/" + config.kitId + ".js";
+      tk.async = true;
+      tk.onload = tk.onreadystatechange = function() {
+        a = this.readyState;
+        if (f || (a && a != "complete" && a != "loaded")) return;
+        f = true;
+        clearTimeout(t);
+        try {
+          Typekit.load(config);
+        } catch (e) {}
+      };
+      s.parentNode.insertBefore(tk, s);
+    })(document);
   }
 };
 </script>
