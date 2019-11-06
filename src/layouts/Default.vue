465bf4a8e3ca5626a84e4dad79e071d95b9d80b1
Lasse Diercks <github@lassediercks.de>
2019-11-06 20:12:00 +0100

    load fonts on next tick

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -122,36 +122,37 @@ export default {
     let vh = window.innerHeight * 0.01;
     // Then we set the value in the --vh custom property to the root of the document
     document.documentElement.style.setProperty("--vh", `${vh}px`);
-
-    (function(d) {
-      var config = {
-          kitId: "uaf4juj",
-          scriptTimeout: 3000,
-          async: true
-        },
-        h = d.documentElement,
-        t = setTimeout(function() {
-          h.className =
-            h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
-        }, config.scriptTimeout),
-        tk = d.createElement("script"),
-        f = false,
-        s = d.getElementsByTagName("script")[0],
-        a;
-      h.className += " wf-loading";
-      tk.src = "https://use.typekit.net/" + config.kitId + ".js";
-      tk.async = true;
-      tk.onload = tk.onreadystatechange = function() {
-        a = this.readyState;
-        if (f || (a && a != "complete" && a != "loaded")) return;
-        f = true;
-        clearTimeout(t);
-        try {
-          Typekit.load(config);
-        } catch (e) {}
-      };
-      s.parentNode.insertBefore(tk, s);
-    })(document);
+    this.$nextTick(e =>
+      (function(d) {
+        var config = {
+            kitId: "uaf4juj",
+            scriptTimeout: 3000,
+            async: true
+          },
+          h = d.documentElement,
+          t = setTimeout(function() {
+            h.className =
+              h.className.replace(/\bwf-loading\b/g, "") + " wf-inactive";
+          }, config.scriptTimeout),
+          tk = d.createElement("script"),
+          f = false,
+          s = d.getElementsByTagName("script")[0],
+          a;
+        h.className += " wf-loading";
+        tk.src = "https://use.typekit.net/" + config.kitId + ".js";
+        tk.async = true;
+        tk.onload = tk.onreadystatechange = function() {
+          a = this.readyState;
+          if (f || (a && a != "complete" && a != "loaded")) return;
+          f = true;
+          clearTimeout(t);
+          try {
+            Typekit.load(config);
+          } catch (e) {}
+        };
+        s.parentNode.insertBefore(tk, s);
+      })(document)
+    );
   }
 };
 </script>
