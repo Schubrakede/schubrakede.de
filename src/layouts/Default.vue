72104a3da3b2e2de698b5ab4d4f68ac345d57557
Lasse Diercks <github@lassediercks.de>
2019-11-08 18:42:51 +0100

    fix sw

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -121,6 +121,23 @@ export default {
   },
 
   mounted() {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", function() {
+        navigator.serviceWorker.register("/sw.js").then(
+          function(registration) {
+            // Registration was successful
+            console.log(
+              "ServiceWorker registration successful with scope: ",
+              registration.scope
+            );
+          },
+          function(err) {
+            // registration failed :(
+            console.log("ServiceWorker registration failed: ", err);
+          }
+        );
+      });
+    }
     this.$nextTick(e =>
       (function(d) {
         var config = {
--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,5 @@
 module.exports = {
   globDirectory: "dist/",
-  globPatterns: ["**/*.{html,css,js,png,json}"],
+  globPatterns: ["**/*.{html,css,js,png,json,svg}"],
   swDest: "dist/sw.js"
 };
